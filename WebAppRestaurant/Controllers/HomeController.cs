fee966def110eaab66653c58ef3147700b4e9bbf
geethabsp07@gmail.com <geethabsp07@gmail.com>
2021-03-12 04:14:25 -0800

    Add project files.

---
+++ WebAppRestaurant/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppRestaurant.Models;
+using WebAppRestaurant.Repositories;
+
+namespace WebAppRestaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RestaurantDBEntities objRestaurantDBEntities;
+        public HomeController()
+        {
+            objRestaurantDBEntities = new RestaurantDBEntities();
+        }
+        public ActionResult Index()
+        {
+            CustomerRepository objCustomerRepository = new CustomerRepository();
+            ItemRepository objItemRepository = new ItemRepository();
+            PaymentTypeRepository objPaymentTypeRepository = new PaymentTypeRepository();
+
+            var objMultipleModels = new Tuple<IEnumerable<SelectListItem>, IEnumerable<SelectListItem>, IEnumerable<SelectListItem>>
+                        (objCustomerRepository.GetAllCustomers(), objItemRepository.GetAllItems(), objPaymentTypeRepository.GetAllPaymentType());
+
+            return View(objMultipleModels);
+        }
+        [HttpGet]
+        public JsonResult getItemUnitPrice(int itemId)
+        {
+            decimal UnitPrice = objRestaurantDBEntities.Items.Single(model => model.ItemId == itemId).ItemPrice;
+            return Json(UnitPrice, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
